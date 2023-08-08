fa02a8f9009bd536c3316e67d5a21dbd28b292d3
Pranav Wanwe <pranavwanwe@gmail.com>
2023-08-08 17:04:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Bioled Free-Range egg')
